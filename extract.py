6a3419b29226ae3276eb3b5dd7240857d8712ea8
Nemo <me@captnemo.in>
2022-02-05 15:35:15 +0530

    re-generate datasets

--- extract.py
+++ extract.py
@@ -1,16 +1,22 @@
 import cv2
 import numpy as np
 import os, os.path
+import random
 
-face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml')
 
-for i in range(117):
+for i in range(140):
     try:
-        img = cv2.imread('Kejri/{}.jpg'.format(i))
+        img = cv2.imread('Modi/{}.jpg'.format(i))
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         x,y,w,h = face_cascade.detectMultiScale(gray)[0]
         img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         img = img[y:y+h, x:x+w]
-        cv2.imwrite('./Kejri_faces/{}.jpg'.format(i),img)
-    except:
+        if random.random() > 0.2:
+            cv2.imwrite('./Data/Train/{}.jpg'.format(i),img)
+        else:
+            cv2.imwrite('./Data/Validation/{}.jpg'.format(i),img)
+    except Exception as e:
+        print(i)
+        print(e)
         pass
